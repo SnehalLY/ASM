95af282ab77dadc10022f471d4cc2f4a82de734c
Nishantd1402 <130464910+Nishantd1402@users.noreply.github.com>
2024-09-21 21:00:03 +0530

    Update settings.py for render allowed host

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-w(8*4$^o!=_4ordi5%a@b_qoo#svb*tg#8o)*&(@gd(0)o@1==
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['asm-1-nar7.onrender.com']
 
 # Application definition
 

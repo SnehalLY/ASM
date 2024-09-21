dd8b47f913ac337d6a99d32665b85c16cfcb4991
Nishantd1402 <nishantdesale1402@gmail.com>
2024-09-21 21:14:06 +0530

    updated host setting

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-w(8*4$^o!=_4ordi5%a@b_qoo#svb*tg#8o)*&(@gd(0)o@1==
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['asm-1gwz.onrender.com']
 
 # Application definition
 
